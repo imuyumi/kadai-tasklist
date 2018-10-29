8ee51377f34fc22edce40acabc1b347a1f54cb4b
yumi nishimura <yoshikawa.westup@gmail.com>
2018-10-29 14:43:51 +0000

    add col-lg-offset-3

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,7 +4,7 @@
 <div class='container'>
     <div class='row'>
        {!! Form::model($task,['route'=>'tasks.store']) !!}
-        <div class='col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-6'>
+        <div class='col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6'>
             <div class='form-group'>
              {!! Form::label('status','task status') !!}
              {!! Form::select('status',['DONE'=>'DONE','NOT'=>'NOT'],null,['class'=>'form-control']) !!}
